5b7f464b5144adcfe8588facfaf39031d34d5789
Seung Joo <psjoo3515@gmail.com>
2024-06-27 15:22:12 +0900

    PayMentService

---
+++ src/main/java/spring/hellospring/Payment.java
@@ -0,0 +1,68 @@
+package spring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+
+    // 주문번호
+    private Long orderId;
+    // 외국 통화 종류
+    private String currency;
+    // 오국 통화 기준 결제 금액
+    // Double, float 사용 x 결제할때 소숫점 오류 있을수 있기에 BigDecimal 사용 !
+    private BigDecimal foreignCurrencyAmount;
+    // 젹용 환율
+    private BigDecimal exRate;
+    // 원화 환산금액
+    private BigDecimal convertedAmount;
+    // 원화 환산 금액 유효시간
+    private LocalDateTime validUntil;
+
+
+    // 생성자 생성 하는 것의 단점 파라미터로 쭉 나열을 해야 하는데, 이럴 경우에 같은 경우의 타입이 연속으로 나오면 실수할 수 있다.
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
---
+++ src/main/java/spring/hellospring/PaymentService.java
@@ -0,0 +1,43 @@
+package spring.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        // 환윺 가져오기
+        // https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close(); //스트림 열면 꼭 닫아줘야함
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateDate data = mapper.readValue(response, ExRateDate.class);
+        BigDecimal exRate = data.rates().get("KRW");
+
+
+        // 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        // 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30); // 30분 더한다.
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
