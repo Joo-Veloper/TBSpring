f784bb290d8742775ff4eba68db0a1c85b01d2e5
Seung Joo <psjoo3515@gmail.com>
2024-06-28 16:31:36 +0900

    Class separation

--- src/main/java/spring/hellospring/Client.java
+++ src/main/java/spring/hellospring/Client.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new SimpleExRatePaymentService(); // abstract 클래스는 인스턴스를 직접 생성할 수 없지만, 이를 상속받은 구체적인 클래스를 통해 인스턴스를 생성할 수 있습니다
+        PaymentService paymentService = new PaymentService(); // abstract 클래스는 인스턴스를 직접 생성할 수 없지만, 이를 상속받은 구체적인 클래스를 통해 인스턴스를 생성할 수 있습니다
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
