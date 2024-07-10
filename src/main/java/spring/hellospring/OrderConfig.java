b31ce09376a032efcbdc7e70ffe017d55f482084
Seung Joo <psjoo3515@gmail.com>
2024-07-10 10:33:17 +0900

    AOP

--- src/main/java/spring/hellospring/OrderConfig.java
+++ src/main/java/spring/hellospring/OrderConfig.java
@@ -4,16 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import spring.hellospring.data.JdbcOrderRepository;
 import spring.hellospring.order.OrderRepository;
 import spring.hellospring.order.OrderService;
 import spring.hellospring.order.OrderServiceImpl;
-import spring.hellospring.order.OrderServiceTxProxy;
+
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement //트렌잭션 관리 기능
 public class OrderConfig {
     @Bean
     public OrderRepository orderRepository(DataSource dataSource) {
@@ -24,9 +26,6 @@ public class OrderConfig {
             PlatformTransactionManager transactionManager,
             OrderRepository orderRepository
     ){
-        return new OrderServiceTxProxy(
-                new OrderServiceImpl(orderRepository),
-                transactionManager
-        );
+        return new OrderServiceImpl(orderRepository);
     }
 }
